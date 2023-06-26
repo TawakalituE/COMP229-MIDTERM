0218f66c2542e4a914af3a762fe88bcced653325
TawakalituE <134027520+TawakalituE@users.noreply.github.com>
2023-06-25 22:11:36 -0400

    update to db URL

--- server/config/db.js
+++ server/config/db.js
@@ -4,5 +4,7 @@
 
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/book_store"
+  //"URI": "mongodb://localhost/book_store"
+
+  "URI": "mongodb+srv://tadinlewa:s1aNAw6UMJf5K8CD@mongodbserver.ueclzrb.mongodb.net/book_store?retryWrites=true&w=majority"
 };
